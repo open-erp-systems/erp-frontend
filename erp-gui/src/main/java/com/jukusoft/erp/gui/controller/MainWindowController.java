9371d5fc3987dc9e361381225ac95597b5adfd57
JuKu <kuenzel.justin@t-online.de>
2017-11-21 12:19:32 +0100

    integrated menu manager in main window.

--- erp-gui/src/main/java/com/jukusoft/erp/gui/controller/MainWindowController.java
+++ erp-gui/src/main/java/com/jukusoft/erp/gui/controller/MainWindowController.java
@@ -1,8 +1,12 @@
 package com.jukusoft.erp.gui.controller;
 
 import com.jukusoft.erp.gui.javafx.FXMLController;
+import com.jukusoft.erp.gui.menu.MenuManager;
+import com.jukusoft.erp.gui.menu.impl.DefaultMenuManager;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -11,9 +15,26 @@ import java.util.ResourceBundle;
 
 public class MainWindowController implements FXMLController, Initializable {
 
+    @FXML
+    protected MenuBar menuBar;
+
+    //menu manager
+    protected MenuManager menuManager = null;
+
+    protected static final int MAIN_MENU_ID = 1;
+
+    /**
+    * default constructor
+    */
+    public MainWindowController () {
+        //create new menu manager
+        this.menuManager = new DefaultMenuManager();
+    }
+
     @Override
     public void init(Stage stage, Scene scene, Pane pane) {
-
+        //initialize menu
+        this.menuManager.fillMenu(this.menuBar, stage, MAIN_MENU_ID);
     }
 
     @Override
