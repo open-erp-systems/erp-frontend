395823d13ef4ba94ad293f796fc688a6acc9c8b2
JuKu <kuenzel.justin@t-online.de>
2017-11-22 18:14:02 +0100

    changed size of main window.

--- erp-gui/src/main/java/com/jukusoft/erp/gui/window/MainWindow.java
+++ erp-gui/src/main/java/com/jukusoft/erp/gui/window/MainWindow.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class MainWindow extends FXMLWindow {
 
     public MainWindow(Stage stage, String title) {
-        super(stage, title, 800, 600, "./data/fxml/main_window.fxml", new MainWindowController());
+        super(stage, title, 1280, 720, "./data/fxml/main_window.fxml", new MainWindowController());
 
         //maximize window
         stage.setMaximized(true);
